32096127aa0fce95c6b6f4e724abeb2fd528484c
ultravietnamita <juanmanueldaza@gmail.com>
2022-02-02 15:32:15 -0300

    fix index

--- src/modelos/price-track/index.ts
+++ src/modelos/price-track/index.ts
@@ -1,72 +1,76 @@
-export * from './in/operador';
-export * from './mongo/operador';
-export * from './out/operador';
+export * from "./in/operador";
+export * from "./mongo/operador";
+export * from "./out/operador";
 //
-export * from './in/composicion';
-export * from './mongo/composicion';
-export * from './out/composicion';
+export * from "./in/composicion";
+export * from "./mongo/composicion";
+export * from "./out/composicion";
 //
-export * from './in/departamento';
-export * from './mongo/departamento';
-export * from './out/departamento';
+export * from "./in/departamento";
+export * from "./mongo/departamento";
+export * from "./out/departamento";
 //
-export * from './in/empresa';
-export * from './mongo/empresa';
-export * from './out/empresa';
+export * from "./in/empresa";
+export * from "./mongo/empresa";
+export * from "./out/empresa";
 //
-export * from './in/familia-quimica';
-export * from './mongo/familia-quimica';
-export * from './out/familia-quimica';
+export * from "./in/familia-quimica";
+export * from "./mongo/familia-quimica";
+export * from "./out/familia-quimica";
 //
-export * from './in/localidad';
-export * from './mongo/localidad';
-export * from './out/localidad';
+export * from "./in/localidad";
+export * from "./mongo/localidad";
+export * from "./out/localidad";
 //
-export * from './in/pago-relevamiento';
-export * from './mongo/pago-relevamiento';
-export * from './out/pago-relevamiento';
+export * from "./in/pago-relevamiento";
+export * from "./mongo/pago-relevamiento";
+export * from "./out/pago-relevamiento";
 //
-export * from './in/principio-activo';
-export * from './mongo/principio-activo';
-export * from './out/principio-activo';
+export * from "./in/principio-activo";
+export * from "./mongo/principio-activo";
+export * from "./out/principio-activo";
 //
-export * from './in/producto-relevamiento';
-export * from './mongo/producto-relevamiento';
-export * from './out/producto-relevamiento';
+export * from "./in/producto-relevamiento";
+export * from "./mongo/producto-relevamiento";
+export * from "./out/producto-relevamiento";
 //
-export * from './in/producto';
-export * from './mongo/producto';
-export * from './out/producto';
+export * from "./in/producto";
+export * from "./mongo/producto";
+export * from "./out/producto";
 //
-export * from './in/provincia';
-export * from './mongo/provincia';
-export * from './out/provincia';
+export * from "./in/provincia";
+export * from "./mongo/provincia";
+export * from "./out/provincia";
 //
-export * from './in/region';
-export * from './mongo/region';
-export * from './out/region';
+export * from "./in/region";
+export * from "./mongo/region";
+export * from "./out/region";
 //
-export * from './in/relevamiento';
-export * from './mongo/relevamiento';
-export * from './out/relevamiento';
+export * from "./in/relevamiento";
+export * from "./mongo/relevamiento";
+export * from "./out/relevamiento";
 //
-export * from './in/segmento';
-export * from './mongo/segmento';
-export * from './out/segmento';
+export * from "./in/segmento";
+export * from "./mongo/segmento";
+export * from "./out/segmento";
 //
-export * from './in/subsegmento';
-export * from './mongo/subsegmento';
-export * from './out/subsegmento';
+export * from "./in/subsegmento";
+export * from "./mongo/subsegmento";
+export * from "./out/subsegmento";
 //
-export * from './in/subsegmento-propio';
-export * from './mongo/subsegmento-propio';
-export * from './out/subsegmento-propio';
+export * from "./in/subsegmento-propio";
+export * from "./mongo/subsegmento-propio";
+export * from "./out/subsegmento-propio";
 //
-export * from './in/ubicacion-relevamiento';
-export * from './mongo/ubicacion-relevamiento';
-export * from './out/ubicacion-relevamiento';
+export * from "./in/ubicacion-relevamiento";
+export * from "./mongo/ubicacion-relevamiento";
+export * from "./out/ubicacion-relevamiento";
 //
-export * from './in/zona';
-export * from './mongo/zona';
-export * from './out/zona';
+export * from "./in/zona";
+export * from "./mongo/zona";
+export * from "./out/zona";
+//
+export * from "./in/grupo-comparativo";
+export * from "./mongo/grupo-comparativo";
+export * from "./out/grupo-comparativo";
 //
