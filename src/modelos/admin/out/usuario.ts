9b3cdc7d3806359c4ddecc0034e21bafd58f05bf
Tatoh <tatukis@gmail.com>
2022-03-31 09:39:38 -0300

    rol externo

--- src/modelos/admin/out/usuario.ts
+++ src/modelos/admin/out/usuario.ts
@@ -3,37 +3,37 @@ import j2s from 'joi-to-swagger';
 import { IClienteDTO, IClienteDTOValidation } from './cliente';
 
 export const IUsuarioDTOValidation = joi.object<IUsuarioDTO>({
-    _id: joi.string(),
-    activo: joi.boolean(),
-    usuario: joi.string(),
-    nombre: joi.string(),
-    apellido: joi.string(),
-    // clave: joi.string(),
-    idCliente: joi.string(),
-    dni: joi.string(),
-    email: joi.string(),
-    pais: joi.string(),
-    telefono: joi.string(),
-    rol: joi.string().allow('admin', 'consultor', 'relevador'),
-    //
-    cliente: IClienteDTOValidation,
+  _id: joi.string(),
+  activo: joi.boolean(),
+  usuario: joi.string(),
+  nombre: joi.string(),
+  apellido: joi.string(),
+  // clave: joi.string(),
+  idCliente: joi.string(),
+  dni: joi.string(),
+  email: joi.string(),
+  pais: joi.string(),
+  telefono: joi.string(),
+  rol: joi.string().allow('admin', 'consultor', 'relevador', 'externo'),
+  //
+  cliente: IClienteDTOValidation,
 });
 
 export const IUsuarioDTOSwagger = j2s(IUsuarioDTOValidation).swagger;
 
 export interface IUsuarioDTO {
-    _id: string;
-    activo: boolean;
-    usuario: string;
-    nombre: string;
-    apellido: string;
-    // clave: string;
-    idCliente: string;
-    dni: string;
-    email: string;
-    pais: string;
-    telefono: string;
-    rol: string;
-    //
-    cliente?: IClienteDTO;
+  _id: string;
+  activo: boolean;
+  usuario: string;
+  nombre: string;
+  apellido: string;
+  // clave: string;
+  idCliente: string;
+  dni: string;
+  email: string;
+  pais: string;
+  telefono: string;
+  rol: string;
+  //
+  cliente?: IClienteDTO;
 }
